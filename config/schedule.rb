dff86769bffd8626eb2092e6e7603b54c9069534
Alejandro AR <abarcadabra@gmail.com>
2016-10-28 22:21:13 -0500

    woof woof

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
-set :output, {:error => '~/Desktop/z.error.log', :standard => '~/Desktop/z.standard.log'}
+set :output, {:error => '~/z.error.log', :standard => '~/z.standard.log'}
 env :PATH, '~/.rbenv/shims:/usr/local/bin:/usr/bin:/bin'
 env :GEM_PATH, ENV['GEM_PATH']
+env :RACK_ENV, ENV['RACK_ENV']
 
 every 1.day, :at => '12:00 pm' do
   rake "bark"
